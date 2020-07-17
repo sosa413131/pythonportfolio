408ab20d0781e56c8057adac799e3090666105bb
David W Sosa <Sosa.David88@gmail.com>
2020-07-17 08:33:32 -0400

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='./templates', static_folder='./assets')
+
+obj={}
+
+@app.route('/')
+def home():
+    obj["title"]="David W Sosa | Data Analyst"
+    obj["style"]="assets/css/style.css"
+    obj["logic"]="assets/js/logic.js"
+    return render_template('child.html', obj=obj)
+
+if __name__ == "__main__":
+    app.run(debug=True)
